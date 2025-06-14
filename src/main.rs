8d141b52c33c0b713ecc4f8d212ac84fb1c325c3
alxn787 <alenabraham787@gmail.com>
2025-06-15 00:05:37 +0530

    learn

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,18 @@
+struct Rect {
+    width:u32,
+    height:u32
+}
+
+impl Rect {
+    fn area (&self)->u32{
+        return self.width * self.height;
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
-    let vec = vec![1,2,3,4,5];
-    print!("{:?}",vec);
+    let r = Rect{
+        width:10,
+        height:15
+    };
+    print!("{} {}", r.width, r.area())
 }
