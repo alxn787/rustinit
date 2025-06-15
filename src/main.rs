106ea7e2a9e94d8f7fb250f547375d6fcd9b9f6a
alxn787 <alenabraham787@gmail.com>
2025-06-15 13:06:11 +0530

    learn

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,8 @@
-use std::f32::consts::PI;
+use chrono::prelude::*;
 
-enum Shapes {
-    Circle(f32),
-    RecTangle(f32,f32),
-    Square(f32)
-}
 
 fn main(){
-    let shape_circle = Shapes::Circle(10.0);
-    let shape_rect = Shapes::RecTangle(10.0, 15.0);
-    let shape_square = Shapes::Square(10.0);
 
-    println!("{}" , area(&shape_circle));
-    println!("{}" , area(&shape_rect));
-    println!("{}" , area(&shape_square));
-
-}
-
-fn area(shape:&Shapes)->f32{
-    match shape {
-        Shapes::Circle(r) => r * r * PI,
-        Shapes::RecTangle(l,b ) => l * b,
-        Shapes::Square(s) => s * s
-        
-    }
+let utc = Local::now();
+print!("{}" , utc);
 }
