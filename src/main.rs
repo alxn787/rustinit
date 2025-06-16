59a8780afd906ee181d38a65e2cf84b63dd506ad
alxn787 <alenabraham787@gmail.com>
2025-06-16 23:26:21 +0530

    learn

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,23 @@
-use chrono::prelude::*;
+use serde::{Serialize, Deserialize};
+use serde_json::{self};
 
+#[derive(Serialize, Deserialize, Debug)]
+struct Person {
+    name: String,
+    age: u32,
+}
 
-fn main(){
+fn main() {
+    let person = Person {
+        name: String::from("John Doe"),
+        age: 30,
+    };
 
-let utc = Local::now();
-print!("{}" , utc);
+    // Serialize to JSON
+    let json_str = serde_json::to_string(&person).unwrap();
+    println!("Serialized JSON: {}", json_str);
+
+    // Deserialize from JSON
+    let deserialized_person: Person = serde_json::from_str(&json_str).unwrap();
+    println!("Deserialized Person: {:?}", deserialized_person);
 }
