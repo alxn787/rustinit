bf7954b60799c41f78f2842c38a23c3e30e88ae5
alxn787 <alenabraham787@gmail.com>
2025-06-17 12:49:05 +0530

    learn

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,20 @@
 
 fn main (){
     let str1 = String::from("alen");
-    let ans:String;
+    let ans:&String;
 
     {
         let str2 = String::from("alex");
-        ans = get_longer_string(&str1, &str2).to_string();
+        ans = get_longer_string(&str1, &str2);
+            print!("{}", ans);
     }
-    print!("{}", ans);
 
 }
 
-fn get_longer_string<'a, 'b>(s1:&'a String, s2:&'b String)-> &'b String{ 
+fn get_longer_string<'a>(s1:&'a String, s2:&'a String)-> &'a String{ 
     if s1.len()>s2.len(){
-        return &s1;
+        return s1;
     }
-    else {return &s2;}
+    else {return s2;}
 }
 
